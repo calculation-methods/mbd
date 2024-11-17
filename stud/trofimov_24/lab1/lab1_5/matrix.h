6d7f4225acac0e5cb355469df35baf24d5503b6c
Дмитрий Евгеньевич <104323436+pivovarov-mai@users.noreply.github.com>
2024-11-17 14:19:24 +0300

    Initial commit

---
+++ stud/trofimov_24/lab1/lab1_5/matrix.h
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include <vector>
+#ifndef LAB1_1_5MATRIX_H
+
+typedef    std::vector<std::vector<double>> matrix;
+matrix solve(matrix A,double eps0);
+void print_matrix(const matrix& matrix1);
+#endif
