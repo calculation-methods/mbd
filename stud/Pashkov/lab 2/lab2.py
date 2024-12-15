0f676baa000071851ef178b38aacebc6e039088c
pashkov_mishel <cc0710@yandex.ru>
2024-12-15 23:50:52 +0300

    Созданы файлы для ЛР2

---
+++ stud/Pashkov/lab 2/lab2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_excel ('lab2-09.xlsx')
+print (df)
