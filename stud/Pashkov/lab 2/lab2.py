3aef80a6f3654a483ed62214fd9f9ea41ea6bc0d
pashkov_mishel <cc0710@yandex.ru>
2024-12-16 15:07:36 +0300

    Добавил визуализацию загруженных данных из файла exel. Остановился на оценке числа групп (кластеров)

--- stud/Pashkov/lab 2/lab2.py
+++ stud/Pashkov/lab 2/lab2.py
@@ -1,10 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
+
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 
-df = pd.read_excel ('lab2-09.xlsx')
+df = pd.read_excel ("lab2-09.xlsx")
 print (df)
-
-
